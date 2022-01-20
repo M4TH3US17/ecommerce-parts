83fc8df9385ad554338ce9b85bc08db4b7a85fc5
M4TH3US17 <matheusdalvino50@gmail.com>
2022-01-20 15:31:06 -0400

    Client Entity/Resource/Repository/Service

---
+++ src/main/java/com/example/demo/entities/Client.java
@@ -0,0 +1,71 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_clients")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(length = 18)
+	private String contact;
+	
+	public Client() {
+	}
+
+	public Client(Long id, String name, String contact) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.contact = contact;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ src/main/java/com/example/demo/services/ClientService.java
@@ -0,0 +1,46 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Client;
+import com.example.demo.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+	public List<Client> findAll() {
+		return repository.findAll();
+	}
+
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public Client save(Client obj) {
+		return repository.save(obj);
+	}
+
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+
+	public Client update(Long id, Client obj) {
+		Client entity = repository.getById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Client entity, Client obj) {
+		entity.setName(obj.getName());
+		entity.setContact(obj.getContact());
+
+	}
+}
