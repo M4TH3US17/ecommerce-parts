a46aa0acb15ebdabef4895ade97d700e03d880b7
M4TH3US17 <matheusdalvino50@gmail.com>
2022-01-31 15:17:14 -0400

    405 exception handled (Method Not allowed)

--- src/main/java/com/example/demo/resources/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/example/demo/resources/exceptions/ControllerExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -53,4 +54,15 @@ public class ControllerExceptionHandler {
 		});
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
 	}
+	
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorDetails> urlInvalidException(HttpRequestMethodNotSupportedException e){
+		ErrorDetails error = new ErrorDetails();
+		error.setTimestamp(LocalDateTime.now().format(formatter));
+		error.setStatus(HttpStatus.METHOD_NOT_ALLOWED.value());
+		error.setError("Non-existent URL address");
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error);
+	}
+	
+	
 }
